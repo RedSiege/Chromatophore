7da19a21a7f5a9c9358f72b6419c789f3b185a40
Mike <hardwaterhacker@users.noreply.github.com>
2025-03-15 12:12:18 -0500

    Update b64string.py
    Added input as argument, saving to file

--- B64String/b64string.py
+++ B64String/b64string.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 import base64
+import sys
 
-plaintext = open('met.bin', 'rb').read()
+try:
+    plaintext = open(sys.argv[1], "rb").read()
+except:
+    print("File argument needed! %s <raw payload file>" % sys.argv[0])
+    sys.exit()
+
 b64 = base64.b64encode(plaintext)
-print(b64)
+
+with open('b64.txt', 'w') as outfile:
+    outfile.write(str(b64, 'utf-8'))
